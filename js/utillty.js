456bb335d2c78f2076a41b0a3db05fdb950c6cfd
naymurweb <naymurweb@gmail.com>
2022-09-29 19:28:53 +0600

    input value utillty

--- js/utillty.js
+++ js/utillty.js
@@ -9,5 +9,14 @@ function inputField(inputId) {
 function inputFieldStringValue(inputId) {
   let inputField = document.getElementById(inputId);
   let inputFieldValue = inputField.value.toLowerCase();
+  inputField.value = "";
+  return inputFieldValue;
+}
+
+// input field number value
+function inputFieldValue(inputId) {
+  let inputField = document.getElementById(inputId);
+  let inputFieldValue = parseFloat(inputField.value);
+  inputField.value = "";
   return inputFieldValue;
 }
