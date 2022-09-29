b232c03fd08d3b9cb93c33724ad6675673e11ad0
naymurweb <naymurweb@gmail.com>
2022-09-29 20:04:41 +0600

    deposit file add

--- js/deposit.js
+++ js/deposit.js
@@ -1,4 +1,3 @@
-// deposit
 document.getElementById("deposit-btn").addEventListener("click", function () {
   let depositInput = inputField("deposit-input");
   let totalBalance = inputField("balance-total");
@@ -10,8 +9,10 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
   //   error handel
   if (isNaN(depositInputValue) || depositInputValue < 0) {
     depositInput.style.border = "2px solid red";
+    depositInput.setAttribute("placeholder", "Please valid deposit amount!");
   } else {
     depositInput.style.border = "none";
+    depositInput.setAttribute("placeholder", "Amount you want to Deposit");
     //   total deposit add
     deposit.innerText = depositAmount + depositInputValue;
     //   total balance add
