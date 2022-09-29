03df5fced9293f1062ea196d0764580827408e87
naymurweb <naymurweb@gmail.com>
2022-09-29 18:58:29 +0600

    input field string value utillty

---
+++ js/utillty.js
@@ -0,0 +1,6 @@
+// input field string value
+function inputFieldStringValue(inputId) {
+  let inputField = document.getElementById(inputId);
+  let inputFieldValue = inputField.value.toLowerCase();
+  return inputFieldValue;
+}
