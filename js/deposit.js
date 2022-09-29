74b1a1c5d99d7fd6446be5cd306ad79db92e6b1b
naymurweb <naymurweb@gmail.com>
2022-09-29 20:38:44 +0600

    withdraw calculation and file add

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,7 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
   let depositInput = inputField("deposit-input");
   let totalBalance = inputField("balance-total");
+  let depositText = document.getElementById("deposit");
 
   let depositInputValue = inputFieldValue("deposit-input");
   let depositAmount = amountValue("deposit");
@@ -14,7 +15,7 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
     depositInput.style.border = "none";
     depositInput.setAttribute("placeholder", "Amount you want to Deposit");
     //   total deposit add
-    deposit.innerText = depositAmount + depositInputValue;
+    depositText.innerText = depositAmount + depositInputValue;
     //   total balance add
     totalBalance.innerText = totalBalanceAmount + depositInputValue;
   }
---
+++ js/withdraw.js
@@ -0,0 +1,22 @@
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  let withdrawInput = inputField("withdraw-input");
+  let totalBalance = inputField("balance-total");
+  let withdrawText = document.getElementById("withdraw");
+
+  let withdrawInputValue = inputFieldValue("withdraw-input");
+  let withdrawAmount = amountValue("withdraw");
+  let totalBalanceAmount = amountValue("balance-total");
+
+  //   error handel
+  if (isNaN(withdrawInputValue) || withdrawInputValue < 0) {
+    withdrawInput.style.border = "2px solid red";
+    withdrawInput.setAttribute("placeholder", "Please valid withdraw amount!");
+  } else {
+    withdrawInput.style.border = "none";
+    withdrawInput.setAttribute("placeholder", "Amount you want to Withdraw");
+    //   total deposit add
+    withdrawText.innerText = withdrawAmount + withdrawInputValue;
+    //   total balance add
+    totalBalance.innerText = totalBalanceAmount - withdrawInputValue;
+  }
+});
